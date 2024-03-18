d7e473c6bab75dca7953bc84fbcc445fa3dd38ab
LinuxLalith <docs.work.gooogle@gmail.com>
2024-03-18 13:52:04 +0530

    changed app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ const app = express();
 const port = 3000;
 
 app.get('/',(req,res)=>{
-    res.json({success : true});
+    res.json({success : true,msg:"you can alter the url to this : http://localhost:3000/search?py=spiderMan&Lvideos=24  OR http://127.0.0.0:3000/search?py=spiderMan&Lvideos=24  to see results" });
 })
 app.get('/search',async(req,res)=>{
-    const {qy,Lvideos} = req.query;
+    const {qy="belugaCat",Lvideos=25} = req.query;
     const youtubeRes = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${Lvideos}&q=${qy}&key=${process.env.YT_API_KEY}`)
     res.json(youtubeRes.data);
 })
