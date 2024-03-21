16af4647999d800ba45b73441e6a7538ea10f612
LinuxLalith <docs.work.gooogle@gmail.com>
2024-03-21 19:17:56 +0530

    json responds only with video emmbed ids

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import 'dotenv/config'
 import express from 'express'
 import axios from 'axios'
+import cors from 'cors'
 const app = express();
-
 const port = 3000;
+app.use(cors({origin : 'http://localhost:5173'}));
 
 app.get('/',(req,res)=>{
     res.json({success : true,msg:"you can alter the url to this : http://localhost:3000/search?py=spiderMan&Lvideos=24  OR http://127.0.0.0:3000/search?py=spiderMan&Lvideos=24  to see results" });
@@ -11,7 +12,12 @@ app.get('/',(req,res)=>{
 app.get('/search',async(req,res)=>{
     const {qy="belugaCat",Lvideos=25} = req.query;
     const youtubeRes = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=${Lvideos}&q=${qy}&key=${process.env.YT_API_KEY}`)
-    res.json(youtubeRes.data);
+
+    const filtered_data = youtubeRes.data.items.map((i,j)=>{
+        if(i.id.videoId) return i.id.videoId;
+    }).filter((i,j)=>i != null);
+    
+    res.json({videoIds : filtered_data});
 })
 
 
